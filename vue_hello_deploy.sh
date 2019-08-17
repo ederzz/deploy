4ba665ae142cd291c81e689ed6e0cece970628ad
shenyiling <jzx19951028yyjy@163.com>
2019-08-17 13:24:22 +0800

    fix(vue_hello_deploy): add build script

--- vue_hello_deploy.sh
+++ vue_hello_deploy.sh
@@ -1,5 +1,7 @@
 cd /root/git_pro/vue-hello-world && git pull # git pull project
 
+npm run build # build
+
 docker build -t vue-hello:latest . # build image
 
 vue_hello_container_id=`docker ps -qaf name=vue-hello` # find container id
